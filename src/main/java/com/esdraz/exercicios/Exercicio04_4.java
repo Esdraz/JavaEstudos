8e21ffadca02c4b348267f9b2a483c1d0aacf3c5
Francisco <fsousa.jpa@gmail.com>
2024-04-13 13:23:54 -0300

    add: Exercicio04_4

---
+++ src/main/java/com/esdraz/exercicios/Exercicio04_4.java
@@ -0,0 +1,36 @@
+package com.esdraz.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04_4 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numOriginal = input.nextInt();
+
+        int numPendenteInversao = numOriginal;
+        int numInvertido = 0;
+
+        while (numPendenteInversao > 0) {
+            int resto = numPendenteInversao % 10;
+            numInvertido = numInvertido * 10 + resto;
+            numPendenteInversao /= 10;
+        }
+        System.out.println("Inverso de " + numOriginal + " é " + numInvertido);
+
+        /*
+        int numero = 7631;
+        int numeroInvertido = 0;
+
+        while (numero > 0) {
+            numeroInvertido *= 10;
+            numeroInvertido += (numero % 10);
+            numero /= 10;
+        }
+
+        System.out.println("Número invertido: " + numeroInvertido);*/
+    }
+}
